55827a74b91ffda241c127bcb201daaffa85bcf7
CultureDevOps <chossat.sebastien@gmail.com>
2024-12-13 16:46:20 +0100

    Fix header

--- components/navigation/Header.tsx
+++ components/navigation/Header.tsx
@@ -40,7 +40,8 @@ const Header = () => {
             </div>
           </Link>
         </div>
-        <div className="flex items-center space-x-4 leading-5 sm:space-x-6 snap-always snap-center">
+        <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+        {/* <div className="flex items-center space-x-4 leading-5 sm:space-x-6 snap-always snap-center"></div> */}
           {headerNavLinks
             .filter((link) => {
               return link.href
