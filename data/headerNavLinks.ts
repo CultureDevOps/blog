36cbbdf04ca54c95438db0db4cee386087707213
CultureDevOps <chossat.sebastien@gmail.com>
2024-12-13 17:19:30 +0100

    Fix header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,5 @@
 const headerNavLinks: { href?: string; title: string }[] = [
-  { href: '/', title: 'Home' },
+  { href: '/landing', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
