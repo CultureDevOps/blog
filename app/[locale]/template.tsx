aae7eab586d680aa202e03b6f782fa2217ba90ee
CultureDevOps <chossat.sebastien@gmail.com>
2024-12-15 10:53:53 +0100

    fix: transitions on mobile

--- app/[locale]/template.tsx
+++ app/[locale]/template.tsx
@@ -1,8 +1,24 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  const isMobileQuery = useMediaQuery({ maxWidth: 768 });
+
+  useEffect(() => {
+    // Après l'hydratation, mettre à jour l'état `isMobile`
+    setIsMobile(isMobileQuery);
+  }, [isMobileQuery]);
+
+  if (isMobile) {
+    console.log("Mobile return children")
+    return <>{children}</>;
+  }
+  console.log("Desktop return motion")
   return (
     <motion.div
       initial={{ y: 15, opacity: 0 }}
