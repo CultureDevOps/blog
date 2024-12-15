b1ae522e3d2fdc3bd17f462aad035f8a8e80d43a
CultureDevOps <chossat.sebastien@gmail.com>
2024-12-15 10:15:40 +0100

    fix: LCP

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -2,7 +2,6 @@ import 'css/tailwind.css'
 import 'pliny/search/algolia.css'
 
 import { Open_Sans } from 'next/font/google'
-import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
 import { SearchProvider } from '@/components/search/SearchProvider'
 import Header from '@/components/navigation/Header'
@@ -15,16 +14,11 @@ import { Metadata } from 'next'
 import { dir } from 'i18next'
 import { LocaleTypes, locales } from './i18n/settings'
 import TwSizeIndicator from '@/components/helper/TwSizeIndicator'
+import Head from 'next/head'
 
 export async function generateStaticParams() {
   return locales.map((locale) => ({ locale }))
 }
-
-const space_grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-space-grotesk',
-})
 
 const open_sans = Open_Sans({
   weight: '400',
@@ -86,37 +80,59 @@ export default function RootLayout({
   params: { locale: LocaleTypes }
 }) {
   return (
+    <><Head>
+      <link
+        rel="preload"
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400&display=swap"
+        as="font"
+        type="font/woff2"
+        crossOrigin="anonymous" />
+      <link
+        rel="preload"
+        href="/css/tailwind.css"
+        as="style"
+        type="text/css"
+        crossOrigin="anonymous"
+      />
+      <link
+        rel="preload"
+        href="/static/images/logo-small.png"
+        as="image"
+        type="image/jpeg"
+        crossOrigin="anonymous"
+      />      
+    </Head>
     <html
       lang={locale}
       dir={dir(locale)}
       className={`${open_sans.variable} scroll-smooth snap-y snap-proximity overflow-scroll`}
       suppressHydrationWarning
     >
-      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
-      <link rel="icon" type="image/png" sizes="192x192" href="/static/favicons/android-chrome-192x192.png" />
-      <link rel="manifest" href="/static/favicons/site.webmanifest" />
-      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-      <meta name="msapplication-TileColor" content="#000000" />
-      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <body className="bg-stone-100 pl-[calc(100vw-100%)] text-black antialiased dark:bg-slate-900 dark:text-white">
-        <TwSizeIndicator />
-        <ThemeProvider>
-          <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
-          <SectionContainer>
-            <div className="flex h-screen flex-col justify-between font-sans">
-              <SearchProvider>
-                <Header />
-                <main className="mb-auto">{children}</main>
-              </SearchProvider>
-              <Footer />
-            </div>
-          </SectionContainer>
-        </ThemeProvider>
-      </body>
-    </html>
+        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/static/favicons/android-chrome-192x192.png" />
+        <link rel="manifest" href="/static/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#000000" />
+        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+        <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+        <body className="bg-stone-100 pl-[calc(100vw-100%)] text-black antialiased dark:bg-slate-900 dark:text-white">
+          <TwSizeIndicator />
+          <ThemeProvider>
+            <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
+            <SectionContainer>
+              <div className="flex h-screen flex-col justify-between font-sans">
+                <SearchProvider>
+                  <Header />
+                  <main className="mb-auto">{children}</main>
+                </SearchProvider>
+                <Footer />
+              </div>
+            </SectionContainer>
+          </ThemeProvider>
+        </body>
+      </html></>
   )
 }
