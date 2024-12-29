057b7dd0dd4fd89ed9191a551d4a3229b7c3c195
CultureDevOps <chossat.sebastien@gmail.com>
2024-12-29 19:05:10 +0100

    Fix: force webp in custom loader

--- components/loader/cloudfrontLoader.ts
+++ components/loader/cloudfrontLoader.ts
@@ -7,7 +7,7 @@ export interface CloudfrontLoaderProps {
 export default function cloudfrontLoader({ src, width, quality }: CloudfrontLoaderProps): string {
   const url = new URL(`${process.env.CLOUD_FRONT_URL}${src}`);
   if (!url.searchParams.has('format')) {
-    url.searchParams.set('format', 'auto'); // Format automatique (WebP, PNG, etc.)
+    url.searchParams.set('format', 'webp'); // Format automatique (WebP, PNG, etc.)
   }
   url.searchParams.set('width', (width || 800).toString()); // Largeur par défaut
   url.searchParams.set('quality', (quality || 90).toString()); // Qualité par défaut
