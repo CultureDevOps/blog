2d8703633c5667b58c4191f41114f78cacd54029
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-02 16:07:56 +0100

    Fix: twitter card

--- app/[locale]/blog/[...slug]/page.tsx
+++ app/[locale]/blog/[...slug]/page.tsx
@@ -102,7 +102,7 @@ export async function generateMetadata({
       card: 'summary_large_image',
       title: post.title,
       description: post.summary,
-      images: imageList,
+      images: ogImages,
     },
   }
 }
