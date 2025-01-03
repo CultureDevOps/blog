67a54c79def5454351cbb911ec0ac36e8d67823d
CultureDevOps <chossat.sebastien@gmail.com>
2025-01-03 09:50:09 +0100

    Fix: avif and quality conflict

--- components/loader/cloudfrontLoader.ts
+++ components/loader/cloudfrontLoader.ts
@@ -8,8 +8,10 @@ export default function cloudfrontLoader({ src, width, quality }: CloudfrontLoad
   const url = new URL(`${process.env.CLOUD_FRONT_URL}${src}`);
   if (!url.searchParams.has('format')) {
     url.searchParams.set('format', 'auto'); // Format automatique (WebP, PNG, etc.)
-  }
+  }  
   url.searchParams.set('width', (width || 800).toString()); // Largeur par défaut
-  url.searchParams.set('quality', (quality || 90).toString()); // Qualité par défaut
+  if (url.searchParams.get('format') !== "avif"){
+    url.searchParams.set('quality', (quality || 90).toString()); // Qualité par défaut
+  }  
   return url.href;
 }
